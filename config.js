dea1551d92f60176d538cfb45204b6a6601c5ccc
Regina Campos <rmscampos@gmail.com>
2020-02-18 15:09:19 -0600

    adds config/app and api keys

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: 'r7Rw0chlStGwRMhteQ45xs3aw',
+    consumer_secret: 'Bd9tWUphritNpAiHq9SlhVbligioXlryZuz2YjMfQ9liZxPOlR',
+    access_token_key: '14939973-1KhYnh4Ez9vayr27oluNON1Y4XqolFl9FoYEmUPhK',
+    access_token_secret: 'PuzePrqqMNtCvnieoD5Csjjkzx45EPbBlCxpqmxZ2rqNq'
+    }
